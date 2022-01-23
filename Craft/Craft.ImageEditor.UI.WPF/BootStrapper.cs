4086d3726046b464818520abb4936ca59916ff70
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-01-23 13:25:41 +0100

    -

---
+++ Craft/Craft.ImageEditor.UI.WPF/BootStrapper.cs
@@ -0,0 +1,16 @@
+﻿using StructureMap;
+using Craft.ImageEditor.ViewModel;
+
+namespace Craft.ImageEditor.UI.WPF
+{
+    public class BootStrapper
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                return Container.For<MainWindowViewModelRegistry>().GetInstance<MainWindowViewModel>();
+            }
+        }
+    }
+}
---
+++ Craft/Craft.ImageEditor.UI.WPF/ImageEditorView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Craft.ImageEditor.ViewModel;
+using Craft.UI.Utils;
+using Craft.Utils;
+
+namespace Craft.ImageEditor.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for ImageEditorView.xaml
+    /// </summary>
+    public partial class ImageEditorView : UserControl
+    {
+        private PointD _mouseDownViewport;
+        private PointD _initialScrollOffset;
+        private bool _dragging;
+
+        private ImageEditorViewModel ViewModel
+        {
+            get { return DataContext as ImageEditorViewModel; }
+        }
+
+        public ImageEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
