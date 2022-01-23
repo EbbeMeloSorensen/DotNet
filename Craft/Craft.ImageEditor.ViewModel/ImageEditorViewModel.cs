971f963c101340ed4e2327af653fbb0883563f4b
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2022-01-23 13:02:39 +0100

    -

---
+++ Craft/Craft.ImageEditor.ViewModel/ImageEditorViewModel.cs
@@ -0,0 +1,101 @@
+﻿using Craft.Utils;
+using GalaSoft.MvvmLight;
+
+namespace Craft.ImageEditor.ViewModel
+{
+    public class ImageEditorViewModel : ViewModelBase
+    {
+        private double _imageWidth;
+        private double _imageHeight;
+        private PointD _scrollableOffset;
+        private PointD _scrollOffset;
+        private double _magnification;
+        private PointD _mouseWorldPosition;
+
+        public double ImageWidth
+        {
+            get { return _imageWidth; }
+            set
+            {
+                if (value.Equals(_imageWidth)) return;
+                _imageWidth = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public double ImageHeight
+        {
+            get { return _imageHeight; }
+            set
+            {
+                if (value.Equals(_imageHeight)) return;
+                _imageHeight = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public PointD ScrollableOffset
+        {
+            get { return _scrollableOffset; }
+            set
+            {
+                if (value.Equals(_scrollableOffset)) return;
+                _scrollableOffset = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public PointD ScrollOffset
+        {
+            get { return _scrollOffset; }
+            set
+            {
+                if (value.Equals(_scrollOffset)) return;
+                _scrollOffset = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public double Magnification
+        {
+            get { return _magnification; }
+            set
+            {
+                if (value.Equals(_magnification)) return;
+                _magnification = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public PointD MouseWorldPosition
+        {
+            get { return _mouseWorldPosition; }
+            set
+            {
+                if (value.Equals(_mouseWorldPosition)) return;
+                _mouseWorldPosition = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ImageEditorViewModel()
+        {
+            ImageWidth = 0;
+            ImageHeight = 0;
+            ScrollOffset = new PointD { X = 0, Y = 0 };
+            ScrollOffset = new PointD { X = 0, Y = 0 };
+            Magnification = 1;
+        }
+
+        public ImageEditorViewModel(
+            int initialImageWidth,
+            int initialImageHeight)
+        {
+            ImageWidth = initialImageWidth;
+            ImageHeight = initialImageHeight;
+            ScrollOffset = new PointD { X = 0, Y = 0 };
+            ScrollOffset = new PointD { X = 0, Y = 0 };
+            Magnification = 1;
+        }
+    }
+}
