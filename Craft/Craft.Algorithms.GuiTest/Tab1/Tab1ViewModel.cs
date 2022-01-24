8515224a93429186cd86834e1d3e77c74ac9ce19
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-01-24 08:29:21 +0100

    Added dependency to Microsoft.Xaml.Behaviours.Wpf from Craft.Algorithms.GuiTest in order to wire up mouse events directly from xaml file to view model without utilizing code behind.

---
+++ Craft/Craft.Algorithms.GuiTest/Tab1/Tab1ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Craft.Algorithms.GuiTest.Tab1
+{
+    public enum Mode
+    {
+        Source,
+        Forbidden
+    }
+
+    public class Tab1ViewModel
+    {
+        private Mode _currentMode = Mode.Source;
+        private HashSet<int> _sourceIndexes;
+        private HashSet<int> _forbiddenIndexes;
+        //private List<PixelViewModel> _pixelViewModels;
+
+    }
+}
