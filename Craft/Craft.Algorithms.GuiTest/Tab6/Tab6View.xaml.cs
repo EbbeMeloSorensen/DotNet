4c84b72136fc1f1aa7a4184fa967a207fc9c918a
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-01-24 19:37:42 +0100

    Completed Craft.Algorithms.GuiTest

---
+++ Craft/Craft.Algorithms.GuiTest/Tab6/Tab6View.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Craft.Algorithms.GuiTest.Tab6
+{
+    /// <summary>
+    /// Interaction logic for Tab6View.xaml
+    /// </summary>
+    public partial class Tab6View : UserControl
+    {
+        public Tab6View()
+        {
+            InitializeComponent();
+        }
+    }
+}
