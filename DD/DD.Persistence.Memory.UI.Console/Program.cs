f6d0618ef1021371437e3562a8455a171254fe30
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-01-27 17:45:54 +0100

    Working on changing Target framework from .Net Core 3.1 to .Net 6.0 (because it is working on the Linux laptop)

--- DD/DD.Persistence.Memory.UI.Console/Program.cs
+++ DD/DD.Persistence.Memory.UI.Console/Program.cs
@@ -1,53 +1,46 @@
-﻿using System.Linq;
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
 using DD.Domain;
+using DD.Persistence.Memory;
 
-namespace DD.Persistence.Memory.UI.Console
+UnitOfWorkFactory _unitOfWorkFactory = new UnitOfWorkFactory();
+
+void CreateCreatureType(
+    string name)
 {
-    internal class Program
+    using (var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork())
     {
-        private static UnitOfWorkFactory _unitOfWorkFactory;
+        var creatureType = new CreatureType(name, 8, 7, 17, 0, 6, null);
 
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("Fun with the DD.Persistence.Memory library");
-
-            _unitOfWorkFactory = new UnitOfWorkFactory();
-
-            CreateCreatureType("Goblin");
-            CountAllCreatureTypes();
-            ListAllCreatureTypes();
-
-            System.Console.WriteLine("Done");
-        }
-
-        static void CreateCreatureType(
-            string name)
-        {
-            using (var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork())
-            {
-                var creatureType = new CreatureType(name, 8, 7, 17, 0, 6, null);
-
-                unitOfWork.CreatureTypes.Add(creatureType);
-                unitOfWork.Complete();
-            }
-        }
-
-        static void CountAllCreatureTypes()
-        {
-            using (var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork())
-            {
-                System.Console.WriteLine($"Creatures in repository: {unitOfWork.CreatureTypes.CountAll()}");
-            }
-        }
-
-        static void ListAllCreatureTypes()
-        {
-            using (var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork())
-            {
-                unitOfWork.CreatureTypes.GetAll()
-                    .ToList()
-                    .ForEach(c => System.Console.WriteLine(c.Name));
-            }
-        }
+        unitOfWork.CreatureTypes.Add(creatureType);
+        unitOfWork.Complete();
     }
 }
+
+void CountAllCreatureTypes()
+{
+    using (var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork())
+    {
+        System.Console.WriteLine($"Creatures in repository: {unitOfWork.CreatureTypes.CountAll()}");
+    }
+}
+
+void ListAllCreatureTypes()
+{
+    using (var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork())
+    {
+        unitOfWork.CreatureTypes.GetAll()
+            .ToList()
+            .ForEach(c => System.Console.WriteLine(c.Name));
+    }
+}
+
+
+Console.WriteLine("Fun with the DD.Persistence.Memory library");
+CreateCreatureType("Zombie");
+CountAllCreatureTypes();
+ListAllCreatureTypes();
+Console.WriteLine("Done");
+
