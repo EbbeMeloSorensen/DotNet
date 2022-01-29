e761230a9249683430afc93ee09f038a8436d1fd
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-01-29 07:26:27 +0100

    Completed adding DD.UI.WPF

---
+++ DD/DD.UI.WPF/ActOutSceneView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DD.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for ActOutSceneView.xaml
+    /// </summary>
+    public partial class ActOutSceneView : UserControl
+    {
+        public ActOutSceneView()
+        {
+            InitializeComponent();
+        }
+    }
+}
