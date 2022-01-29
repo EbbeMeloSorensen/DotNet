950e137b5a3becbe57cf8059d832c2f474de1a51
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2022-01-29 17:25:21 +0100

    Completed DMI.StatDB.UI.WPF

---
+++ DMI/DMI.StatDB/DMI.StatDB.UI.WPF/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DMI.StatDB.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+    }
+}
