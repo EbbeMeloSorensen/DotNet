245f2cf1a14059d7d21b645d9d83b0d3af0602ba
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-01-29 09:27:58 +0100

    Added DMI.SMS.UI.WPF

---
+++ DMI/DMI.SMS/DMI.SMS.UI.WPF/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DMI.SMS.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+    }
+}
---
+++ DMI/DMI.SMS/DMI.SMS.UI.WPF/BootStrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using StructureMap;
+using DMI.SMS.ViewModel;
+
+namespace DMI.SMS.UI.WPF
+{
+    public class BootStrapper
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                try
+                {
+                    var mainWindowViewModel = Container.For<MainWindowViewModelRegistry>().GetInstance<MainWindowViewModel>();
+
+                    return mainWindowViewModel;
+                }
+                catch (ConfigurationErrorsException)
+                {
+                    Console.WriteLine("Error reading app settings");
+                    throw;
+                }
+            }
+        }
+    }
+}
