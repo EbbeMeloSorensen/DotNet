8c3e75c61ec3b1a02040181f8d4cbe215cdded24
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-02-09 21:16:34 +0100

    -

--- DMI/DMI.SMS/DMI.SMS.Application/Application.cs
+++ DMI/DMI.SMS/DMI.SMS.Application/Application.cs
@@ -24,6 +24,45 @@ namespace DMI.SMS.Application
 
     public class Application
     {
+        private static Dictionary<int, string> _blackListedStationIds = new Dictionary<int, string>
+        {
+            //{ 6052, "Thyborøn (There is quite a bit of confusion about this one. Ib says it has been owned by DMI since 1961, the according to the sms database, it has been owned by Kystdirektoratet in a period of time). Julia Sommer has so far decided to omit it" },
+            { 6189, "DMI Giws Test" },
+            { 34231, "Mittarfik Kangerlussuaq (According to Jens Q it is ours until 1st Jan 2020, but Julia says we shouldn't include it)" },
+            { 34234, "Mittarfik Sisimiut (According to Jens Q we are in doubt, so we exclude it)" },
+            { 34310, "Station Nord (According to Jens Q we are in doubt, so we exclude it)" },
+            //{ 20552, "Års Syd (Julia, PO of Frie Data decided to omit this station mid December 2019)" },
+            //{ 28280, "Årslev (Replaced by Højby. Julia, PO of Frie Data decided to omit this station mid December 2019)" },
+            //{ 32175, "Østerlars (Julia, PO of Frie Data decided to omit this station mid December 2019)" },
+            { 06055, "Torsminde (Det er en Synop ejet af KDI, som vi ikke udstiller (vi udstiller kun deres vandstandsstationer)" },
+            { 04405, "Qaanaaq Isvp Vest (Tilføjet efter launch af v2)" },
+            { 04406, "Qaanaaq Isvp Øst (Tilføjet efter launch af v2)" },
+            { 05126, "Karup Vest (Tilføjet efter launch af v2)" },
+            { 05127, "Karup Øst (Tilføjet efter launch af v2)" },
+            { 21100, "Vestervig (fordi den er genaktiveret, men vi låser lige på releasen fra efteråret 2020)" },
+            { 25152, "Esbjerg Havn 0 - teststation ifølge Ib"},
+            { 30344, "Kastrup Havn - teststation ifølge Ib"},
+            { 25151, "Esbjerg Havn || - den skal vi ikka have med endnu (13-04-2021)" }
+        };
+
+        public static Dictionary<int, List<int>> _blackListedStationRowsIdentifiedByObjectId = new Dictionary<int, List<int>>
+        {
+            { 6051, new List<int>{ 102091, 102092 } },                                                 // Vestervig
+            //{ 6079, new List<int>{ 102177, 102178, 102179, 102180 } },                               // Anholt Havn
+            { 6116, new List<int>{ 102273 } },                                                         // Store Jyndevad
+            { 6147, new List<int>{ 102325, 102326, 102327, 102329, 102332, 102337, 102338, 102341 } }, // Vindebæk Kyst
+            { 6181, new List<int>{ 102447 } },                                                         // Jægersborg
+            { 6183, new List<int>{ 102451, 102452, 102454 } },                                         // Drogden Fyr
+            { 4220, new List<int>{ 512 } },                                                            // Aasiaat
+            { 4320, new List<int>{ 550 } },                                                            // Danmarkshavn
+            { 4339, new List<int>{ 377 } },                                                            // Ittoqqortoormiit
+            { 4360, new List<int>{ 346 } },                                                            // Roskilde
+            { 30414, new List<int>{ 53371 } },                                                         //
+            { 24380, new List<int>{ 53376 } },                                                         // Tarm
+            { 31063, new List<int>{ 11770 } },                                                         // Rørvig
+            { 32175, new List<int>{ 53361, 53362, 53365 } },                                           // Østerlars
+        };
+
         private IUIDataProvider _uiDataProvider;
         private ILogger _logger;
 
@@ -199,8 +238,21 @@ namespace DMI.SMS.Application
                 var dateTimeOfInterest = new DateTime(2021, 1, 24, 0, 0, 0);
                 stationDataRaw = stationDataRaw.RollbackToPreviousDate(dateTimeOfInterest);
 
-                // Filter out everything that is not current
+                // Remove records with blacklisted station ids
                 var stationData = stationDataRaw
+                    .Where(row => !(row.StationIDDMI.HasValue && 
+                                  _blackListedStationIds.Keys.Contains(row.StationIDDMI.Value)))
+                    .ToList();
+
+                stationData = stationData
+                    .Where(row => !(
+                        row.StationIDDMI.HasValue && 
+                        _blackListedStationRowsIdentifiedByObjectId.Keys.Contains(row.StationIDDMI.Value) &&
+                        _blackListedStationRowsIdentifiedByObjectId[row.StationIDDMI.Value].Contains(row.ObjectId)))
+                    .ToList();
+
+                // Filter out everything that is not current
+                stationData = stationData
                     .Where(row => row.GdbToDate.Year == 9999)
                     .ToList();
 
@@ -223,7 +275,6 @@ namespace DMI.SMS.Application
                 // Add historical meteorological stations
                 var koldingSneStation = stationData
                     .Single(row => row.StationIDDMI == 23327);
-
 
                 stations.Add(koldingSneStation.ConvertToFrieDataStation(false));
 
