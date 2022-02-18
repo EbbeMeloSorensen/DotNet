d70c162d9e8c730d78e0b704199c43f2264fdf12
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-02-18 21:17:18 +0100

    Working on StatDB console application

---
+++ DMI/DMI.StatDB/DMI.StatDB.UI.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using StructureMap;
+
+namespace DMI.StatDB.UI.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Fun with a DMI.StatDB command line User interface");
+
+            var container = Container.For<InstanceScanner>();
+            //var application = container.GetInstance<Application.Application>();
+            //application.Initialize();
+
+            //System.Console.WriteLine("Counting StationInformation records...");
+            //System.Console.WriteLine($"Station Count: {application.UIDataProvider.GetAllStationInformations().Count}");
+
+            //await MakeBreakfast(application);
+        }
+
+        //private static async Task MakeBreakfast(
+        //    Application.Application application)
+        //{
+        //    System.Console.Write("Making breakfast...\nProgress: ");
+        //    var dateTime = DateTime.Now;
+        //    await application.MakeBreakfast(dateTime, (progress, nameOfSubtask) =>
+        //    {
+        //        System.Console.SetCursorPosition(10, System.Console.CursorTop);
+        //        System.Console.Write($"{progress:F2} %");
+        //        return false;
+        //    });
+        //    System.Console.WriteLine("\nDone");
+        //}
+    }
+}
