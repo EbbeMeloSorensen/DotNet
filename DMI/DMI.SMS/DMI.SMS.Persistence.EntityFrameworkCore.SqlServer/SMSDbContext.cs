cf02414fa4f817c66f2445e35febb83adedbd21d
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-03-15 08:23:44 +0100

    Prepared EntityFrameworkCore (SqlServer) proj

---
+++ DMI/DMI.SMS/DMI.SMS.Persistence.EntityFrameworkCore.SqlServer/SMSDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DMI.SMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMI.SMS.Persistence.EntityFrameworkCore.SqlServer
+{
+    public class SMSDbContext : DbContext
+    {
+        public DbSet<StationInformation> StationInformations { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = ConnectionStringProvider.GetConnectionString();
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
