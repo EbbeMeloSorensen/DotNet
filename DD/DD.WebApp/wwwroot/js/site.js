c405aceaead6a2bc9a38a5c3691f61fc18b31121
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2022-04-14 18:42:39 +0200

    Minimal change that involves calling a simple java script function

--- DD/DD.WebApp/wwwroot/js/site.js
+++ DD/DD.WebApp/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function dummyFunction() {
+    document.getElementById("output").innerHTML = (3.1415927).toFixed(2);
+}
