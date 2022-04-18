a9efbf8171e75d8c83920850f9ee4496fde7879d
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-04-18 15:00:09 +0200

    Working on a simple Web App for DD

--- DD/DD.WebApp.FrontOnly/wwwroot/js/site.js
+++ DD/DD.WebApp.FrontOnly/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var creatureTypes = {};
 
-// Write your JavaScript code.
+function getAllCreatureTypes() {
+    // Mocking for a start
+    creatureTypes.items = new Array();
+
+    creatureTypes.items = [
+        { id: 0, name: "Goblin", armorClass: 7, hitPoints: 5, thaco: 17 },
+        { id: 1, name: "Dragon", armorClass: -1, hitPoints: 72, thaco: 5 }
+    ];
+
+    drawCreatureTypeTable(creatureTypes);
+}
+
+function drawCreatureTypeTable(creatureTypeList) {
+    $tbody = $("#table-body");
+    $tbody.empty();
+    for (var i = 0; i < creatureTypeList.items.length; i++) {
+        $tr = $("<tr>");
+        $("<td>").html(creatureTypeList.items[i].name).appendTo($tr);
+        $("<td>").html(creatureTypeList.items[i].armorClass).appendTo($tr);
+        $("<td>").html(creatureTypeList.items[i].hitPoints).appendTo($tr);
+        $("<td>").html(creatureTypeList.items[i].thaco).appendTo($tr);
+        $tbody.append($tr);
+    }
+}
+
+$(document).ready(function () {
+    console.info("ready");
+    getAllCreatureTypes();
+});
