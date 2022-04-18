88384b6c40476b79dfbe400147d99fa3f29cb7d8
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2022-04-18 15:11:42 +0200

    -

--- DD/DD.WebApp.FrontOnly/wwwroot/js/site.js
+++ DD/DD.WebApp.FrontOnly/wwwroot/js/site.js
@@ -25,6 +25,30 @@ function drawCreatureTypeTable(creatureTypeList) {
     }
 }
 
+//function dummyFunction() {
+//    document.getElementById("output").innerHTML = (3.1415927).toFixed(2);
+
+//    // Dollartegnet indikerer, at vi bruger en funktion fra jquery
+//    $.ajax({
+//        //url: "http://api.exchangeratesapi.io/v1/latest?access_key=3d5861a43985fbc3778eda7aaa2fefa9&symbols=USD,INR",
+//        url: "https://localhost:44358/api/artists",
+//        type: "GET",
+//        dataType: "json",
+//        success: function (result) {
+//            //heroes = result. // "We set our gloal heroes list
+//            //drawHeroTable(result);
+//            //document.getElementById("output").innerHTML = (3.1415927).toFixed(2);
+//        }
+//    });
+//}
+
+// we will use jquery and ajax to make an asynchronous call to the web service. Jquery is a powerful javascript library 
+// which makes many javascript functionality easier, especially when it comes to browser compatibility, You can also
+// access DOM elements with jQuery faster than with plain javascript.
+// Ajax is just a set of web development techniques that allows us to do the upcoming calls to our web service. Ajax
+// stands for "Asyncrhonous Javascript and XML"
+
+
 $(document).ready(function () {
     console.info("ready");
     getAllCreatureTypes();
--- DD/DD.WebApp/Readme.txt
+++
@@ -1,4 +0,0 @@
-﻿Dette er indtil videre en ren instans af project templaten ASP.NET Web App baseret på .Net Core 6.0.
-Bemærk, at der allerede er noget css og js i applikationen fra starten af
-
-Den skal gerne bringes til at trække på Web API'et fra DD.WebAPI-projektet
