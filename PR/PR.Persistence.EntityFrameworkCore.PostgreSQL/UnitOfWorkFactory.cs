4d29fa1bb6fd343ca61bccda770ce4fa7c2f48db
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-09-11 19:47:40 +0200

    Got the postgreSQL plugin to work

--- PR/PR.Persistence.EntityFrameworkCore.PostgreSQL/UnitOfWorkFactory.cs
+++ PR/PR.Persistence.EntityFrameworkCore.PostgreSQL/UnitOfWorkFactory.cs
@@ -1,9 +1,21 @@
 ﻿using Craft.Logging;
+using Microsoft.EntityFrameworkCore;
+using PR.Domain.Entities;
 
 namespace PR.Persistence.EntityFrameworkCore.PostgreSQL
 {
     public class UnitOfWorkFactory : IUnitOfWorkFactory
     {
+        static UnitOfWorkFactory()
+        {
+            using var context = new PRDbContext();
+            context.Database.EnsureCreated();
+
+            if (context.People.Any()) return;
+
+            SeedDatabase(context);
+        }
+
         public void Initialize(ILogger logger)
         {
         }
@@ -16,6 +28,28 @@ namespace PR.Persistence.EntityFrameworkCore.PostgreSQL
         public IUnitOfWork GenerateUnitOfWork()
         {
             return new UnitOfWork(new PRDbContext());
+        }
+
+        private static void SeedDatabase(DbContext context)
+        {
+            var people = new List<Person>
+            {
+                new Person
+                {
+                    FirstName = "Kasper"
+                },
+                new Person
+                {
+                    FirstName = "Jesper"
+                },
+                new Person
+                {
+                    FirstName = "Jonathan"
+                }
+            };
+
+            context.AddRange(people);
+            context.SaveChanges();
         }
     }
 }
