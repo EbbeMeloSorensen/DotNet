00def25cd6cf7595ccb01f6525e72db6f2b323c2
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-09-11 19:38:52 +0200

    Getting there with new shot at a postgreSQL plugin

---
+++ PR/PR.Persistence.EntityFrameworkCore.PostgreSQL/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using PR.Persistence.Repositories;
+using PR.Persistence.EntityFrameworkCore.PostgreSQL.Repositories;
+
+namespace PR.Persistence.EntityFrameworkCore.PostgreSQL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly PRDbContext _context;
+
+        public IPersonRepository People { get; }
+
+        public UnitOfWork(PRDbContext context)
+        {
+            _context = context;
+            People = new PersonRepository(_context);
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ PR/PR.Persistence.EntityFrameworkCore.PostgreSQL/UnitOfWorkFactory.cs
@@ -0,0 +1,21 @@
+﻿using Craft.Logging;
+
+namespace PR.Persistence.EntityFrameworkCore.PostgreSQL
+{
+    public class UnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        public void Initialize(ILogger logger)
+        {
+        }
+
+        public Task<bool> CheckRepositoryConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IUnitOfWork GenerateUnitOfWork()
+        {
+            return new UnitOfWork(new PRDbContext());
+        }
+    }
+}
