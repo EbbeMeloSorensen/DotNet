b16a72ebe2fefbd877cded123f4b4fba953479fc
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-09-11 07:57:57 +0200

    Getting there with the sqlite plugin

---
+++ PR/PR.Persistence.EntityFrameworkCore.Sqlite/Migrations/20220911055029_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PR.Persistence.EntityFrameworkCore.Sqlite.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
