aa5bed80a25ab0f00f3485a5599f710642b7b756
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-09-23 20:39:31 +0200

    Working on WPF App for PR

---
+++ PR/PR.UI.WPF/BootStrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using PR.ViewModel;
+using StructureMap;
+
+namespace PR.UI.WPF
+{
+    public class BootStrapper
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                try
+                {
+                    var mainWindowViewModel = Container.For<MainWindowViewModelRegistry>().GetInstance<MainWindowViewModel>();
+
+                    return mainWindowViewModel;
+                }
+                catch (ConfigurationErrorsException)
+                {
+                    Console.WriteLine("Error reading app settings");
+                    throw;
+                }
+            }
+        }
+    }
+}
