9481347baa532acb7c19fdea523d61a6016efa3c
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2023-04-25 18:25:37 +0200

    Prepared a new console app for inspecting elevation angles

---
+++ DMI/DMI.DAL/ElevationAngleInspector/Program.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+
+var host = "";
+var user = "";
+var password = "";
+var database = "";
+var connectionString = $"Host={host};Username={user};Password={password};Database={database}";
+
+using var conn = new NpgsqlConnection(connectionString);
+conn.Open();
+
+try
+{
+    var query = "SELECT statid FROM station";
+
+    using (var cmd = new NpgsqlCommand(query, conn))
+    using (var reader = cmd.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            if (!reader.IsDBNull(0))
+            {
+                var statid = reader.GetInt32(0);
+
+                //result.Add(statid);
+                Console.WriteLine("hej");
+            }
+        }
+    }
+}
+catch (PostgresException excp)
+{
+    throw excp;
+}
