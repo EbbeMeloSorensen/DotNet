054ee462e3b13eb1977357fad50bd825f080e977
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-05-19 20:18:46 +0200

    -

--- Craft/Craft.DataStructures.IO.UnitTest/GeographyIOTest.cs
+++ Craft/Craft.DataStructures.IO.UnitTest/GeographyIOTest.cs
@@ -229,5 +229,110 @@ namespace Craft.DataStructures.IO.UnitTest
             // Act
             DataIOHandler.SerializeGMLFeature(featureCollection, outputFile);
         }
+
+        [Fact]
+        public void WriteDataToGMLFile_Denmark()
+        {
+            // Arrange
+            var featureCollection = new FeatureCollection
+            {
+                FeatureCollectionElements = new ArrayList
+                {
+                    new BoundedBy
+                    {
+                        Envelope = new Envelope
+                        {
+                            SrsName = "urn:ogc:def:crs:EPSG::4326",
+                            LowerCorner = "54.5685897308149 8.09522545663074",
+                            UpperCorner = "57.7475040838931 15.1513777996996"
+                        }
+                    },
+                    new FeatureMember
+                    {
+                        Dummy = new Dummy
+                        {
+                            Id = "Dummy.0",
+                            BoundedBy = new BoundedBy
+                            {
+                                Envelope = new Envelope
+                                {
+                                    SrsName = "urn:ogc:def:crs:EPSG::4326",
+                                    LowerCorner = "54.5685897308149 8.09522545663074",
+                                    UpperCorner = "57.7475040838931 15.1513777996996"
+                                }
+                            },
+                            GeometryProperty = new GeometryProperty
+                            {
+                                AbstractGeometricPrimitive = new MultiSurface
+                                {
+                                    Id = "Dummy.geom.0",
+                                    SrsName = "urn:ogc:def:crs:EPSG::4326",
+                                    SurfaceMembers = new List<SurfaceMember>
+                                    {
+                                        new SurfaceMember
+                                        {
+                                            AbstractSurface = new Polygon
+                                            {
+                                                Id = "Dummy.geom.0.0",
+                                                Exterior = new Exterior
+                                                {
+                                                    LinearRing = new LinearRing
+                                                    {
+                                                        PosList = new PosList
+                                                        {
+                                                            value = "55.1332054686372 15.1513777996996 55.0871035865293 15.1447046234481 55.0446638033151 15.1066186859134 55.0182948742212 15.113242231028 54.9916379760827 15.0726662222297 55.0136918820187 14.9272651740024 55.0511283276872 14.7808327260851 55.101346806941 14.6841726868696 55.2358259138106 14.7092391289766 55.3092715427146 14.7609155598443 55.2481998608834 14.8514222052358 55.2157987261239 14.9267990988856 55.2161318986776 14.9733992844557 55.1884219443294 15.0004988942938 55.1332054686372 15.1513777996996"
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        new SurfaceMember
+                                        {
+                                            AbstractSurface = new Polygon
+                                            {
+                                                Id = "Dummy.geom.0.1",
+                                                Exterior = new Exterior
+                                                {
+                                                    LinearRing = new LinearRing
+                                                    {
+                                                        PosList = new PosList
+                                                        {
+                                                            value = "55.3454450542716 8.4764103520195 55.3397484402929 8.45167076902097 55.3800316317211 8.40235436219948 55.4274356150119 8.37094160207172 55.4632836005446 8.37435957132228 55.4684105334975 8.40821373689635 55.4387067557721 8.4119572257739 55.4274356150119 8.46338951907252 55.3933779967167 8.45020592497985 55.3454450542716 8.4764103520195"
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        new SurfaceMember
+                                        {
+                                            AbstractSurface = new Polygon
+                                            {
+                                                Id = "Dummy.geom.0.2",
+                                                Exterior = new Exterior
+                                                {
+                                                    LinearRing = new LinearRing
+                                                    {
+                                                        PosList = new PosList
+                                                        {
+                                                            value = "55.0302188160742 12.4171655609038 55.0023867860642 12.5376082690068 54.9571800534988 12.5517684222214 54.9600284845324 12.3483992950617 54.886175940012 12.2077742943654 54.9072938916941 12.115000834841 54.9408226232353 12.1490991514734 54.9544130667591 12.1291609952626 54.9680849698803 12.1770125600863 54.9892031271413 12.1770125593969 54.9755312377468 12.2321883388978 54.9892031271413 12.2868758403373 55.0233828810922 12.2942000661125 55.0438500009283 12.2526961598901 55.0607363938512 12.2509871748822 55.0642764355563 12.2800399101602 55.0332705745751 12.3352970709562 55.0302188160742 12.4171655609038"
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                            Name = "Denmark"
+                        }
+                    }
+                }
+            };
+
+            var outputFile = @"C:\Temp\Denmark.gml";
+
+            // Act
+            DataIOHandler.SerializeGMLFeature(featureCollection, outputFile);
+        }
     }
 }
