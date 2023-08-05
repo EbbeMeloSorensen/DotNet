9f6fa14c92914f5e84ba3df294a5646b1cb2349d
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-08-05 11:17:57 +0200

    Housekeeping

--- Glossary/Glossary.IO.UnitTest/DataIOHandlerTest.cs
+++ Glossary/Glossary.IO.UnitTest/DataIOHandlerTest.cs
@@ -14,13 +14,13 @@ namespace Glossary.IO.UnitTest
         public void ExportDataToXML_Works()
         {
             new DataIOHandler()
-                .ExportDataToXML(GenerateDataSet(), @"C:/Temp/GlossaryData.xml");
+                .ExportDataToXML(GenerateDataSet(), "Temp.xml");
         }
 
         [Fact]
         public void ImportDataFromXML_Works()
         {
-            new DataIOHandler().ImportDataFromXML(@"C:/Temp/GlossaryData.xml", out GlossaryData glossaryData);
+            new DataIOHandler().ImportDataFromXML(@"Data/Glossary.xml", out GlossaryData glossaryData);
 
             glossaryData.Records.Count.Should().Be(3);
             glossaryData.Records.Count(p => p.Term == "Kafka").Should().Be(1);
@@ -30,14 +30,14 @@ namespace Glossary.IO.UnitTest
         [Fact]
         public void ExportDataToJson_Works()
         {
-            new DataIOHandler().ExportDataToJson(GenerateDataSet(), @"C:/Temp/GlossaryData.json");
+            new DataIOHandler().ExportDataToJson(GenerateDataSet(), "Temp.json");
         }
 
         [Fact]
         public void ImportDataFromJson_Works()
         {
             var dataIOHandler = new DataIOHandler();
-            dataIOHandler.ImportDataFromJson(@"C:/Temp/GlossaryData.json", out var glossaryData);
+            dataIOHandler.ImportDataFromJson(@"Data/Glossary.json", out var glossaryData);
 
             glossaryData.Records.Count.Should().Be(3);
             glossaryData.Records.Count(p => p.Term == "Kafka").Should().Be(1);
