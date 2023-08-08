e8429bf0ac018264330bfab94a0cc27c1cadc7a2
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-08-08 14:23:35 +0200

    -

--- Craft/Craft.UIElements/TrafficLight/TrafficLightView.xaml.cs
+++ Craft/Craft.UIElements/TrafficLight/TrafficLightView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Craft.UIElements.Common
+namespace Craft.UIElements.TrafficLight
 {
     /// <summary>
     /// Interaction logic for TrafficLightView.xaml
