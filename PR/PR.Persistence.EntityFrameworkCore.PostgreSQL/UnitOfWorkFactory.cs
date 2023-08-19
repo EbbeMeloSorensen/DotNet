261e71423ac8fd7d33bb02d297b11c9f64d2add8
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-08-19 09:15:20 +0200

    House keeping

--- PR/PR.Persistence.EntityFrameworkCore.PostgreSQL/UnitOfWorkFactory.cs
+++ PR/PR.Persistence.EntityFrameworkCore.PostgreSQL/UnitOfWorkFactory.cs
@@ -41,67 +41,224 @@ namespace PR.Persistence.EntityFrameworkCore.PostgreSQL
 
         private static void SeedDatabase(DbContext context)
         {
-            var person1 = new Person
+            // Star Wars
+            // https://cdn.shopify.com/s/files/1/1835/6621/files/star-wars-family-tree-episde-9.png?v=1578255836
+
+            var now = DateTime.UtcNow;
+            var delay = 0;
+
+            var luke = new Person
             {
-                FirstName = "Uffe",
-                Surname = "Sørensen",
-                Created = new DateTime(2022, 1, 1, 3, 3, 3).ToUniversalTime()
+                FirstName = "Luke",
+                Surname = "Skywalker",
+                Created = now + new TimeSpan(delay++)
             };
 
-            var person2 = new Person
+            var leia = new Person
             {
-                FirstName = "Ebbe",
-                Surname = "Melo Sørensen",
-                Created = new DateTime(2022, 1, 1, 3, 3, 4).ToUniversalTime()
+                FirstName = "Leia",
+                Surname = "Organa",
+                Created = now + new TimeSpan(delay++),
             };
 
-            var personAssociation = new PersonAssociation
+            var anakin = new Person
             {
-                SubjectPerson = person1,
-                ObjectPerson = person2,
-                Description = "is the brother of",
-                Created = new DateTime(2022, 1, 1, 3, 3, 3).ToUniversalTime()
+                FirstName = "Anakin",
+                Surname = "Skywalker",
+                Nickname = "Darth Vader",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var obiVan = new Person
+            {
+                FirstName = "Obi Van",
+                Surname = "Kenobi",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var padme = new Person
+            {
+                FirstName = "Padme",
+                Surname = "Amidala",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var benSolo = new Person
+            {
+                FirstName = "Ben",
+                Surname = "Solo",
+                Nickname = "Kylo Ren",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var hanSolo = new Person
+            {
+                FirstName = "Han",
+                Surname = "Solo",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var quiGon = new Person
+            {
+                FirstName = "Qui-Gon",
+                Surname = "Jinn",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var countDooku = new Person
+            {
+                FirstName = "Count Dooku",
+                Nickname = "Darth Tyranus",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var palpatine = new Person
+            {
+                FirstName = "Emperor Palpatine",
+                Nickname = "Darth Sidious",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var rey = new Person
+            {
+                FirstName = "Rey",
+                Surname = "Skywalker",
+                Created = now + new TimeSpan(delay++),
             };
 
             var people = new List<Person>
             {
-                new Person
-                {
-                    FirstName = "Uffe",
-                    Surname = "Sørensen",
-                    Created = new DateTime(2022, 1, 1, 3, 3, 3).ToUniversalTime()
-                },
-                new Person
-                {
-                    FirstName = "Tina",
-                    Surname = "Gosman",
-                    Created = new DateTime(2022, 1, 1, 3, 3, 4).ToUniversalTime()
-                },
-                new Person
-                {
-                    FirstName = "Ebbe",
-                    Created = new DateTime(2022, 1, 1, 3, 3, 5).ToUniversalTime()
-                },
-                new Person
-                {
-                    FirstName = "Ana Tayze",
-                    Surname = "Melo Sørensen",
-                    Nickname = "Hamos",
-                    Address = "Danshøjvej 33",
-                    ZipCode = "2500",
-                    City = "Valby",
-                    Birthday = new DateTime(1980, 6, 13).ToUniversalTime(),
-                    Category = "Familie",
-                    Description = "Min kone",
-                    Dead = false,
-                    Created = new DateTime(2022, 1, 1, 3, 3, 6).ToUniversalTime()
-                }
+                luke,
+                leia,
+                anakin,
+                obiVan,
+                padme,
+                benSolo,
+                hanSolo,
+                quiGon,
+                countDooku,
+                palpatine,
+                rey
             };
 
-            //context.AddRange(people);
-            context.Add(person1);
-            context.Add(person2);
-            context.Add(personAssociation);
+            var personAssociations = new List<PersonAssociation>
+            {
+                new()
+                {
+                    SubjectPerson = anakin,
+                    Description = "is married with",
+                    ObjectPerson = padme,
+                    Created = now + new TimeSpan(delay),
+                },
+                new()
+                {
+                    SubjectPerson = anakin,
+                    Description = "is a parent of",
+                    ObjectPerson = luke,
+                    Created = now + new TimeSpan(delay + 1),
+                },
+                new()
+                {
+                    SubjectPerson = anakin,
+                    Description = "is a parent of",
+                    ObjectPerson = leia,
+                    Created = now + new TimeSpan(delay + 2),
+                },
+                new()
+                {
+                    SubjectPerson = padme,
+                    Description = "is a parent of",
+                    ObjectPerson = luke,
+                    Created = now + new TimeSpan(delay + 3),
+                },
+                new()
+                {
+                    SubjectPerson = padme,
+                    Description = "is a parent of",
+                    ObjectPerson = leia,
+                    Created = now + new TimeSpan(delay + 4),
+                },
+                new()
+                {
+                    SubjectPerson = leia,
+                    Description = "is married with",
+                    ObjectPerson = hanSolo,
+                    Created = now + new TimeSpan(delay + 5),
+                },
+                new()
+                {
+                    SubjectPerson = leia,
+                    Description = "is a parent of",
+                    ObjectPerson = benSolo,
+                    Created = now + new TimeSpan(delay + 6),
+                },
+                new()
+                {
+                    SubjectPerson = hanSolo,
+                    Description = "is a parent of",
+                    ObjectPerson = benSolo,
+                    Created = now + new TimeSpan(delay + 7),
+                },
+                new()
+                {
+                    SubjectPerson = luke,
+                    Description = "is an apprentice of",
+                    ObjectPerson = obiVan,
+                    Created = now + new TimeSpan(delay + 8),
+                },
+                new()
+                {
+                    SubjectPerson = obiVan,
+                    Description = "is an apprentice of",
+                    ObjectPerson = quiGon,
+                    Created = now + new TimeSpan(delay + 9),
+                },
+                new()
+                {
+                    SubjectPerson = quiGon,
+                    Description = "is an apprentice of",
+                    ObjectPerson = countDooku,
+                    Created = now + new TimeSpan(delay + 10)
+                },
+                new()
+                {
+                    SubjectPerson = countDooku,
+                    Description = "is an apprentice of",
+                    ObjectPerson = palpatine,
+                    Created = now + new TimeSpan(delay + 11)
+                },
+                new()
+                {
+                    SubjectPerson = anakin,
+                    Description = "is an apprentice of",
+                    ObjectPerson = palpatine,
+                    Created = now + new TimeSpan(delay + 12)
+                },
+                new()
+                {
+                    SubjectPerson = palpatine,
+                    Description = "is a grandparent of",
+                    ObjectPerson = rey,
+                    Created = now + new TimeSpan(delay + 13)
+                },
+                new()
+                {
+                    SubjectPerson = rey,
+                    Description = "is an apprentice of",
+                    ObjectPerson = luke,
+                    Created = now + new TimeSpan(delay + 14)
+                },
+                new()
+                {
+                    SubjectPerson = rey,
+                    Description = "is an apprentice of",
+                    ObjectPerson = leia,
+                    Created = now + new TimeSpan(delay + 15)
+                },
+            };
+
+            context.AddRange(people);
+            context.AddRange(personAssociations);
             context.SaveChanges();
         }
     }
--- PR/PR.Persistence.EntityFrameworkCore.Sqlite/UnitOfWorkFactory.cs
+++ PR/PR.Persistence.EntityFrameworkCore.Sqlite/UnitOfWorkFactory.cs
@@ -32,28 +32,224 @@ namespace PR.Persistence.EntityFrameworkCore.Sqlite
 
         private static void SeedDatabase(DbContext context)
         {
+            // Star Wars
+            // https://cdn.shopify.com/s/files/1/1835/6621/files/star-wars-family-tree-episde-9.png?v=1578255836
+
             var now = DateTime.UtcNow;
+            var delay = 0;
+
+            var luke = new Person
+            {
+                FirstName = "Luke",
+                Surname = "Skywalker",
+                Created = now + new TimeSpan(delay++)
+            };
+
+            var leia = new Person
+            {
+                FirstName = "Leia",
+                Surname = "Organa",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var anakin = new Person
+            {
+                FirstName = "Anakin",
+                Surname = "Skywalker",
+                Nickname = "Darth Vader",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var obiVan = new Person
+            {
+                FirstName = "Obi Van",
+                Surname = "Kenobi",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var padme = new Person
+            {
+                FirstName = "Padme",
+                Surname = "Amidala",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var benSolo = new Person
+            {
+                FirstName = "Ben",
+                Surname = "Solo",
+                Nickname = "Kylo Ren",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var hanSolo = new Person
+            {
+                FirstName = "Han",
+                Surname = "Solo",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var quiGon = new Person
+            {
+                FirstName = "Qui-Gon",
+                Surname = "Jinn",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var countDooku = new Person
+            {
+                FirstName = "Count Dooku",
+                Nickname = "Darth Tyranus",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var palpatine = new Person
+            {
+                FirstName = "Emperor Palpatine",
+                Nickname = "Darth Sidious",
+                Created = now + new TimeSpan(delay++),
+            };
+
+            var rey = new Person
+            {
+                FirstName = "Rey",
+                Surname = "Skywalker",
+                Created = now + new TimeSpan(delay++),
+            };
 
             var people = new List<Person>
             {
-                new Person
+                luke,
+                leia,
+                anakin,
+                obiVan,
+                padme,
+                benSolo,
+                hanSolo,
+                quiGon,
+                countDooku,
+                palpatine,
+                rey
+            };
+
+            var personAssociations = new List<PersonAssociation>
+            {
+                new()
                 {
-                    FirstName = "Kasper",
-                    Created = now,
+                    SubjectPerson = anakin,
+                    Description = "is married with",
+                    ObjectPerson = padme,
+                    Created = now + new TimeSpan(delay),
                 },
-                new Person
+                new()
                 {
-                    FirstName = "Jesper",
-                    Created = now + new TimeSpan(1),
+                    SubjectPerson = anakin,
+                    Description = "is a parent of",
+                    ObjectPerson = luke,
+                    Created = now + new TimeSpan(delay + 1),
                 },
-                new Person
+                new()
                 {
-                    FirstName = "Jonathan",
-                    Created = now + new TimeSpan(2),
-                }
+                    SubjectPerson = anakin,
+                    Description = "is a parent of",
+                    ObjectPerson = leia,
+                    Created = now + new TimeSpan(delay + 2),
+                },
+                new()
+                {
+                    SubjectPerson = padme,
+                    Description = "is a parent of",
+                    ObjectPerson = luke,
+                    Created = now + new TimeSpan(delay + 3),
+                },
+                new()
+                {
+                    SubjectPerson = padme,
+                    Description = "is a parent of",
+                    ObjectPerson = leia,
+                    Created = now + new TimeSpan(delay + 4),
+                },
+                new()
+                {
+                    SubjectPerson = leia,
+                    Description = "is married with",
+                    ObjectPerson = hanSolo,
+                    Created = now + new TimeSpan(delay + 5),
+                },
+                new()
+                {
+                    SubjectPerson = leia,
+                    Description = "is a parent of",
+                    ObjectPerson = benSolo,
+                    Created = now + new TimeSpan(delay + 6),
+                },
+                new()
+                {
+                    SubjectPerson = hanSolo,
+                    Description = "is a parent of",
+                    ObjectPerson = benSolo,
+                    Created = now + new TimeSpan(delay + 7),
+                },
+                new()
+                {
+                    SubjectPerson = luke,
+                    Description = "is an apprentice of",
+                    ObjectPerson = obiVan,
+                    Created = now + new TimeSpan(delay + 8),
+                },
+                new()
+                {
+                    SubjectPerson = obiVan,
+                    Description = "is an apprentice of",
+                    ObjectPerson = quiGon,
+                    Created = now + new TimeSpan(delay + 9),
+                },
+                new()
+                {
+                    SubjectPerson = quiGon,
+                    Description = "is an apprentice of",
+                    ObjectPerson = countDooku,
+                    Created = now + new TimeSpan(delay + 10)
+                },
+                new()
+                {
+                    SubjectPerson = countDooku,
+                    Description = "is an apprentice of",
+                    ObjectPerson = palpatine,
+                    Created = now + new TimeSpan(delay + 11)
+                },
+                new()
+                {
+                    SubjectPerson = anakin,
+                    Description = "is an apprentice of",
+                    ObjectPerson = palpatine,
+                    Created = now + new TimeSpan(delay + 12)
+                },
+                new()
+                {
+                    SubjectPerson = palpatine,
+                    Description = "is a grandparent of",
+                    ObjectPerson = rey,
+                    Created = now + new TimeSpan(delay + 13)
+                },
+                new()
+                {
+                    SubjectPerson = rey,
+                    Description = "is an apprentice of",
+                    ObjectPerson = luke,
+                    Created = now + new TimeSpan(delay + 14)
+                },
+                new()
+                {
+                    SubjectPerson = rey,
+                    Description = "is an apprentice of",
+                    ObjectPerson = leia,
+                    Created = now + new TimeSpan(delay + 15)
+                },
             };
 
             context.AddRange(people);
+            context.AddRange(personAssociations);
             context.SaveChanges();
         }
     }
