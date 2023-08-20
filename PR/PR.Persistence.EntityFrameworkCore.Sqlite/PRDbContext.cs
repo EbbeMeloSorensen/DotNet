3d7b95ee7f84ea322b715123dfc0e3260df4340c
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-08-20 20:57:15 +0200

    Getting quite pretty

--- PR/PR.Persistence.EntityFrameworkCore.Sqlite/PRDbContext.cs
+++ PR/PR.Persistence.EntityFrameworkCore.Sqlite/PRDbContext.cs
@@ -1,36 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using PR.Domain.Entities;
-using PR.Persistence.EntityFrameworkCore.EntityConfigurations;
 
 namespace PR.Persistence.EntityFrameworkCore.Sqlite
 {
-    public class PRDbContext : DbContext
+    public class PRDbContext : PRDbContextBase
     {
-        public DbSet<Person> People { get; set; }
-        public DbSet<PersonAssociation> PersonAssociations { get; set; }
-
         protected override void OnConfiguring(
             DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = ConnectionStringProvider.GetConnectionString();
             optionsBuilder.UseSqlite(connectionString);
-        }
-
-        protected override void OnModelCreating(
-            ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfiguration(new PersonConfiguration());
-            modelBuilder.ApplyConfiguration(new PersonAssociationConfiguration());
-
-            modelBuilder.Entity<PersonAssociation>()
-                .HasOne(p => p.SubjectPerson)
-                .WithMany(pa => pa.ObjectPeople)
-                .HasForeignKey(pa => pa.SubjectPersonId);
-
-            modelBuilder.Entity<PersonAssociation>()
-                .HasOne(p => p.ObjectPerson)
-                .WithMany(pa => pa.SubjectPeople)
-                .HasForeignKey(pa => pa.ObjectPersonId);
         }
     }
 }
