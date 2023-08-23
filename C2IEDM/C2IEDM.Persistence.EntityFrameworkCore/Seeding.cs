d0a7d842fb3286e937ce422d406b69d926620857
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-08-23 19:11:32 +0200

    -

--- C2IEDM/C2IEDM.Persistence.EntityFrameworkCore/Seeding.cs
+++ C2IEDM/C2IEDM.Persistence.EntityFrameworkCore/Seeding.cs
@@ -14,9 +14,16 @@ namespace C2IEDM.Persistence.EntityFrameworkCore
                 LongitudeCoordinate = 34.1
             };
 
+            var location2 = new AbsolutePoint
+            {
+                LatitudeCoordinate = 23.2,
+                LongitudeCoordinate = 34.2
+            };
+
             var locations = new List<Location>
             {
-                location1
+                location1,
+                location2
             };
 
             context.AddRange(locations);
