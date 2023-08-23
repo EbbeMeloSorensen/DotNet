aed1e292ccdaa8f08eebe605866212e277fdd651
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2023-08-23 19:33:54 +0200

    -

---
+++ C2IEDM/C2IEDM.Persistence.EntityFrameworkCore.PostgreSQL/C2IEDMDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace C2IEDM.Persistence.EntityFrameworkCore.PostgreSQL;
+
+public class C2IEDMDbContext : C2IEDMDbContextBase
+{
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = ConnectionStringProvider.GetConnectionString();
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+}
---
+++ C2IEDM/C2IEDM.Persistence.EntityFrameworkCore.PostgreSQL/UnitOfWorkFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace C2IEDM.Persistence.EntityFrameworkCore.PostgreSQL;
+
+public class UnitOfWorkFactory : IUnitOfWorkFactory
+{
+    static UnitOfWorkFactory()
+    {
+        using var context = new C2IEDMDbContext();
+        context.Database.EnsureCreated();
+
+        if (context.Locations.Any()) return;
+
+        Seeding.SeedDatabase(context);
+    }
+
+    public IUnitOfWork GenerateUnitOfWork()
+    {
+        return new UnitOfWork(new C2IEDMDbContext());
+    }
+}
