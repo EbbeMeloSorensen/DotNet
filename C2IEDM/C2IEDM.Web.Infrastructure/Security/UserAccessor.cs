70680bb1a21919ef0a45e1f0d81ce0235089ee8b
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-08-27 08:50:24 +0200

    Added C2IEDM.Infrastructure layer

---
+++ C2IEDM/C2IEDM.Web.Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using C2IEDM.Web.Application.Interfaces;
+
+namespace C2IEDM.Web.Infrastructure.Security;
+
+public class UserAccessor : IUserAccessor
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UserAccessor(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public string GetUsername()
+    {
+        return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+    }
+}
