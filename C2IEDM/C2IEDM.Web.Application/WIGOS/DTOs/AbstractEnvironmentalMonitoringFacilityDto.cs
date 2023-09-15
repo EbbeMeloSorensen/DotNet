fcdab473ee863f5d5f1f9c7f7eb0a5500bc43a65
EbbeMeloSorensen <ebs@dmi.dk>
2023-09-15 16:20:44 +0200

    -

---
+++ C2IEDM/C2IEDM.Web.Application/WIGOS/DTOs/AbstractEnvironmentalMonitoringFacilityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace C2IEDM.Web.Application.WIGOS.DTOs;
+
+public class AbstractEnvironmentalMonitoringFacilityDto
+{
+    public string category { get; set; }
+    public Guid id { get; set; }
+
+    public AbstractEnvironmentalMonitoringFacilityDto()
+    {
+    }
+}
---
+++ C2IEDM/C2IEDM.Web.Application/WIGOS/DTOs/ObservingFacilityDto.cs
@@ -0,0 +1,5 @@
+﻿namespace C2IEDM.Web.Application.WIGOS.DTOs;
+
+public class ObservingFacilityDto : AbstractEnvironmentalMonitoringFacilityDto
+{
+}
