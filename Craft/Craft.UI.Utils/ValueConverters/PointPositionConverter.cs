ed04436453a0162e940f2e5972ca17ff972c88ea
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-10-15 08:48:06 +0200

    -

---
+++ Craft/Craft.UI.Utils/ValueConverters/PointPositionConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Craft.UI.Utils.ValueConverters
+{
+    // Used in GeometryEditorView for positioning points
+    public class PointPositionConverter : IMultiValueConverter
+    {
+        public object Convert(
+            object[] values,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            // Hvis den her ikke er der, kan det åbenbart gå i kuk, hvor den prøver at caste et "unset" objekt
+            if (!(values[1] is double))
+            {
+                return 0;
+            }
+
+            var coordinate = (double)values[0];
+            var upperLeft = (double)values[1];
+            var scale = (double)values[2];
+            var extent = (double)values[3];
+            var shift = (double)values[4];
+
+            return (coordinate - upperLeft) * scale - extent / 2 + shift;
+        }
+
+        public object[] ConvertBack(
+            object value,
+            Type[] targetTypes,
+            object parameter,
+            CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
