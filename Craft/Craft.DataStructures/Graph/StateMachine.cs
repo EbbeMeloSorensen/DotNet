d1aff2cfa0572bea22fa56327160c661da5d6e72
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-11-05 10:34:47 +0100

    -

---
+++ Craft/Craft.DataStructures/Graph/StateMachine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Craft.DataStructures.Graph
+{
+    public class StateMachine : GraphAdjacencyList<LabelledVertex, LabelledEdge>
+    {
+        public StateMachine() : base(true)
+        {
+        }
+
+        public StateMachine(
+            IEnumerable<LabelledVertex> vertices) : base(vertices, true)
+        {
+        }
+    }
+}
