c32ee786a213fececf2954a1d2dd3e52a53183c2
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2023-11-18 07:41:34 +0100

    Generated TowerDefense game as a clone of Zelda

---
+++ Simulator/Game.TowerDefense.UI.WPF/CustomizedShapeTemplateSelector.cs
@@ -0,0 +1,34 @@
+﻿using Craft.ViewModels.Geometry2D.ScrollFree;
+using Game.TowerDefense.ViewModel.ShapeViewModels;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Game.TowerDefense.UI.WPF
+{
+    public class CustomizedShapeTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(
+            object item,
+            DependencyObject container)
+        {
+            var element = container as FrameworkElement;
+
+            switch (item)
+            {
+                case ZeldaViewModel _:
+                    {
+                        return element.FindResource("Zelda") as DataTemplate;
+                    }
+                case RectangleViewModel _:
+                    {
+                        return element.FindResource("Wall") as DataTemplate;
+                    }
+                default:
+                    {
+                        throw new ArgumentException("item doesn't correspond to any DataTemplate");
+                    }
+            }
+        }
+    }
+}
