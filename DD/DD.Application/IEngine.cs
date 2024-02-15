ae8d106a54b29be04ded919517ec32277c04ece3
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2024-02-15 11:13:07 +0100

    -

--- DD/DD.Application/IEngine.cs
+++ DD/DD.Application/IEngine.cs
@@ -7,14 +7,6 @@ using DD.Application.BattleEvents;
 
 namespace DD.Application
 {
-    public class MoveCreatureResult
-    {
-        public int? IndexOfDestinationSquare { get; set; }
-        public double? WalkingDistanceToDestinationSquare { get; set; }
-        public Creature FinalClosestOpponent { get; set; }
-        public double FinalDistanceToClosestOpponent { get; set; }
-    }
-
     public interface IEngine
     {
         ILogger Logger { get; set; }
---
+++ DD/DD.Application/MoveCreatureResult.cs
@@ -0,0 +1,12 @@
+﻿using DD.Domain;
+
+namespace DD.Application
+{
+    public class MoveCreatureResult
+    {
+        public int? IndexOfDestinationSquare { get; set; }
+        public double? WalkingDistanceToDestinationSquare { get; set; }
+        public Creature FinalClosestOpponent { get; set; }
+        public double FinalDistanceToClosestOpponent { get; set; }
+    }
+}
