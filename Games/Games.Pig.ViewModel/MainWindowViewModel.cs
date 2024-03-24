3998cafd417c3b1f7b65516693c657554c46f2d2
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2024-03-24 20:58:05 +0100

    -

--- Games/Games.Pig.ViewModel/MainWindowViewModel.cs
+++ Games/Games.Pig.ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Games.Pig.Application;
+﻿using System;
+using GalaSoft.MvvmLight.Command;
+using Games.Pig.Application;
 
 namespace Games.Pig.ViewModel
 {
@@ -6,10 +8,48 @@ namespace Games.Pig.ViewModel
     {
         private Engine _engine;
 
+        private RelayCommand _startGameCommand;
+        private RelayCommand _rollDieCommand;
+        private RelayCommand _takePotCommand;
+
+        public RelayCommand StartGameCommand => _startGameCommand ??= new RelayCommand(StartGame, CanStartGame);
+        public RelayCommand RollDieCommand => _rollDieCommand ??= new RelayCommand(RollDie, CanRollDie);
+        public RelayCommand TakePotCommand => _rollDieCommand ??= new RelayCommand(TakePot, CanTakePot);
+
         public MainWindowViewModel()
         {
             var players = new[] { true, false };
             _engine = new Engine(players);
+        }
+
+        private void StartGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanStartGame()
+        {
+            return true;
+        }
+
+        private void RollDie()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanRollDie()
+        {
+            return true;
+        }
+
+        private void TakePot()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanTakePot()
+        {
+            return true;
         }
     }
 }
