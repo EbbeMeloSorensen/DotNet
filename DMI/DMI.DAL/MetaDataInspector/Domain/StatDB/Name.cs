a34c601345db769a69aab3f9a28ab1c378a964d7
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2024-05-21 21:17:44 +0200

    -

---
+++ DMI/DMI.DAL/MetaDataInspector/Domain/StatDB/Name.cs
@@ -0,0 +1,8 @@
+﻿namespace MetaDataInspector.Domain.StatDB;
+
+public class Name
+{
+    public int? statid { get; set; }
+    public DateTime? start_time { get; set; }
+    public string? name { get; set; }
+}
