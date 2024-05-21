36c8bfef937ab4683a0f6b6ba05e2c5d5d857608
Ebbe Sorensen <ebs@dmi.dk>
2024-05-21 19:45:02 +0200

    -

--- DMI/DMI.DAL/MetaDataInspector/Program.cs
+++ DMI/DMI.DAL/MetaDataInspector/Program.cs
@@ -34,26 +34,26 @@ foreach (var si in stationInformations)
     if (matchingStationCount == 0)
     {
         sw.PrintLine("NO MATCHING STATION IN STATDB");
-        continue;
     }
     else if (matchingStationCount > 1)
     {
         sw.PrintLine("MULTIPLE MATCHING STATIONS IN STATDB");
-        continue;
     }
+    else
+    {
+        var s = matchingStations.Single();
 
-    var s = matchingStations.Single();
+        //sw.PrintLine($"    icao id:                      {si.StationIDICAOAsString,40}");
+        sw.InspectCountry(si, s, true);
+        // sw.PrintLine($"    station id:                   {si.StationIDDMIAsString,40}");
+        // sw.PrintLine($"    datefrom (sms):               {si.DateFromAsString,40}");
+        // sw.PrintLine($"    dateto (sms):                 {si.DateToAsString,40}");
+        // sw.PrintLine($"    station type:                 {si.StationTypeAsString,40}");
+        // sw.PrintLine($"    station owner:                {si.StationOwnerAsString,40}");
 
-    sw.PrintLine($"    icao id:                      {si.StationIDICAOAsString,40}");
-    sw.InspectCountry(si, s, true);
-    sw.PrintLine($"    station id:                   {si.StationIDDMIAsString,40}");
-    sw.PrintLine($"    datefrom (sms):               {si.DateFromAsString,40}");
-    sw.PrintLine($"    dateto (sms):                 {si.DateToAsString,40}");
-    sw.PrintLine($"    station type:                 {si.StationTypeAsString,40}");
-    sw.PrintLine($"    station owner:                {si.StationOwnerAsString,40}");
-
-    // Fra det gamle script
-    //PrintLine(sw, $"    source:                       {"",40} {sourceAsString,40}   ({sourceOK})");
+        // Fra det gamle script
+        //PrintLine(sw, $"    source:                       {"",40} {sourceAsString,40}   ({sourceOK})");
+    }
 
     sw.PrintLine("-----------------------------------------------------------------------------------------------------------------------------------------------");
 }
