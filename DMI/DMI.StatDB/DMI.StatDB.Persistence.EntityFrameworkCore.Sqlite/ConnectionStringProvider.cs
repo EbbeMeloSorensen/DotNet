9fa0b3f98b86293e52bd6234725938f1b72808ee
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2024-06-30 09:58:51 +0200

    -

---
+++ DMI/DMI.StatDB/DMI.StatDB.Persistence.EntityFrameworkCore.Sqlite/ConnectionStringProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace DMI.StatDB.Persistence.EntityFrameworkCore.Sqlite
+{
+    public static class ConnectionStringProvider
+    {
+        private static string _connectionString;
+
+        static ConnectionStringProvider()
+        {
+            _connectionString = "Data source=statdb.db";
+        }
+
+        public static string GetConnectionString()
+        {
+            return _connectionString;
+        }
+    }
+}
