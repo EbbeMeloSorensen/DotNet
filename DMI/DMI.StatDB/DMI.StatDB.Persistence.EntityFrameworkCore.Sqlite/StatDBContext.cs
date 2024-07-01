48964dc14234a15ab037cda39ddb22931b7bf80f
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2024-07-01 17:53:23 +0200

    -

---
+++ DMI/DMI.StatDB/DMI.StatDB.Persistence.EntityFrameworkCore.Sqlite/StatDBContext.cs
@@ -0,0 +1,19 @@
+﻿using DMI.StatDB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMI.StatDB.Persistence.EntityFrameworkCore.Sqlite
+{
+    public class StatDBContext : DbContext
+    {
+        public DbSet<Station> StationInformations { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = ConnectionStringProvider.GetConnectionString();
+            optionsBuilder.UseSqlite(connectionString);
+        }
+
+
+    }
+}
