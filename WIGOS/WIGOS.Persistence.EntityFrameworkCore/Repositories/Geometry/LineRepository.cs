60d291782f7b3e82268abc3743cacdb667e63162
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2024-10-05 08:18:17 +0200

    -

--- WIGOS/WIGOS.Persistence.EntityFrameworkCore/Repositories/Geometry/LineRepository.cs
+++ WIGOS/WIGOS.Persistence.EntityFrameworkCore/Repositories/Geometry/LineRepository.cs
@@ -30,26 +30,33 @@ namespace WIGOS.Persistence.EntityFrameworkCore.Repositories.Geometry
 
         public IList<Line> GetLinesIncludingPoints()
         {
+            // Retrieve all the LATEST version of the line objects that haven't been superceded or deleted
             var lines = DbContext.Lines
                 .Where(_ => _.Superseded == DateTime.MaxValue)
                 .ToList();
 
+            // Identify the object ids of those objects (we need those ids for identifying the relevant line points)
             var lineObjectIds = lines
                 .Select(_ => _.ObjectId)
                 .ToList();
 
+            // Retrieve all the LATEST version of relevant line points
             var linePoints = DbContext.LinePoints
                 .Where(_ => _.Superseded == DateTime.MaxValue && lineObjectIds.Contains(_.LineObjectId))
                 .ToList();
 
+            // As before, identify the ids of the line point objects in order to identify the relevant points
             var pointObjectIds = linePoints
                 .Select(_ => _.PointObjectId)
                 .Distinct()
                 .ToList();
 
+            // Retrieve all the LATEST version of relevant points
             var points = DbContext.Points
                 .Where(_ => _.Superseded == DateTime.MaxValue && pointObjectIds.Contains(_.ObjectId))
                 .ToList();
+
+            // Notice that this function only facilitates retrieval of the LATEST version - so no retrospection here...
 
             var pointMap = points.ToDictionary(_ => _.ObjectId, _ => _);
 
