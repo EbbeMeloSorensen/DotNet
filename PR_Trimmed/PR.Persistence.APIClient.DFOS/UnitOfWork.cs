cdacf654f5ee2b407a047ebf68ea3d0fa7f65ee3
EbbeMeloSorensen <ebs@dmi.dk>
2024-11-01 14:23:09 +0100

    -

---
+++ PR_Trimmed/PR.Persistence.APIClient.DFOS/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using PR.Persistence.APIClient.DFOS.Repositories;
+using PR.Persistence.Repositories;
+using System;
+
+namespace PR.Persistence.APIClient.DFOS
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IPersonRepository People { get; }
+
+        public UnitOfWork(
+            DateTime? historicalTime,
+            DateTime? databaseTime)
+        {
+            People = new PersonRepository(
+                historicalTime,
+                databaseTime);
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Complete()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ PR_Trimmed/PR.Persistence.APIClient.DFOS/UnitOfWorkFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PR.Persistence.APIClient.DFOS
+{
+    public class UnitOfWorkFactory : IUnitOfWorkFactoryVersioned, IUnitOfWorkFactoryHistorical
+    {
+        public DateTime? HistoricalTime { get; set; }
+        public bool IncludeHistoricalObjects { get; set; }
+        public DateTime? DatabaseTime { get; set; }
+
+        public void Initialize(
+            bool versioned)
+        {
+            // Here we would normally have made sure the database existed and possibly seeded it,
+            // but we don't do anything, when the unit of work represents an API
+
+            // We might obtain the token here, though..
+        }
+
+        public IUnitOfWork GenerateUnitOfWork()
+        {
+            return new UnitOfWork(
+                HistoricalTime,
+                DatabaseTime);
+        }
+
+        public void Reseed()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
