c03bf7bf5d41fde2ea991e3fc82e2654aa02bfae
Ebbe Melo Sørensen <ebs@dmi.dk>
2024-11-04 16:26:26 +0100

    -

--- PR_Trimmed/PR.UI.Console/InstanceScanner.cs
+++ PR_Trimmed/PR.UI.Console/InstanceScanner.cs
@@ -1,4 +1,5 @@
 ﻿using StructureMap;
+//using System.Configuration;
 
 namespace PR.UI.Console
 {
@@ -6,11 +7,17 @@ namespace PR.UI.Console
     {
         public InstanceScanner()
         {
+            // Kan ikke umiddelbartfå det til at virke... det lader det ellers til at gøre for WPF-applikationen
+            //var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            //var settings = configFile.AppSettings.Settings;
+            //var repositoryPluginAssembly = settings["RepositoryPluginAssembly"]?.Value;
+
             Scan(_ =>
             {
                 //_.TheCallingAssembly(); // No the implementation of the interface is not in this assembly
-                _.AssembliesAndExecutablesFromApplicationBaseDirectory(); // The implementation of the interface is in another assembly in the base directory
                 _.WithDefaultConventions(); // Default Convention is that the interface has the same name as the implementation except for the I prefix
+                _.AssembliesAndExecutablesFromApplicationBaseDirectory(); // The implementation of the interface is in another assembly in the base directory
+                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("PR.Persistence"));
             });
         }
     }
