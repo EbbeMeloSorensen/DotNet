54fbc2b6d6db8e4b272f37b9c1327494f0c3c565
Ebbe Melo Sørensen <ebs@dmi.dk>
2024-11-04 15:42:59 +0100

    -

--- PR_Trimmed/PR.UI.WPF.GIS/MainWindowViewModelRegistry.cs
+++ PR_Trimmed/PR.UI.WPF.GIS/MainWindowViewModelRegistry.cs
@@ -1,4 +1,5 @@
 ﻿using StructureMap;
+using System.Configuration;
 
 namespace PR.UI.WPF.GIS
 {
@@ -6,12 +7,17 @@ namespace PR.UI.WPF.GIS
     {
         public MainWindowViewModelRegistry()
         {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            var repositoryPluginAssembly = settings["RepositoryPluginAssembly"]?.Value;
+
             Scan(_ =>
             {
                 _.WithDefaultConventions();
                 _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("Craft.Logging"));
                 _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("Craft.UIElements"));
-                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("PR"));
+                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("PR.IO"));
+                _.Assembly(repositoryPluginAssembly);
                 _.LookForRegistries();
             });
         }
