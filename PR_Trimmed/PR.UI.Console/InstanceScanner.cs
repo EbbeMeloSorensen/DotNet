79adaf8124915a6a025a92759c903558a989f267
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2024-11-16 12:12:12 +0100

    -

--- PR_Trimmed/PR.UI.Console/InstanceScanner.cs
+++ PR_Trimmed/PR.UI.Console/InstanceScanner.cs
@@ -1,5 +1,5 @@
-﻿using StructureMap;
-//using System.Configuration;
+﻿using System.Configuration;
+using StructureMap;
 
 namespace PR.UI.Console
 {
@@ -7,17 +7,17 @@ namespace PR.UI.Console
     {
         public InstanceScanner()
         {
-            // Kan ikke umiddelbartfå det til at virke... det lader det ellers til at gøre for WPF-applikationen
-            //var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            //var settings = configFile.AppSettings.Settings;
-            //var repositoryPluginAssembly = settings["RepositoryPluginAssembly"]?.Value;
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            var repositoryPluginAssembly = settings["RepositoryPluginAssembly"]?.Value;
 
             Scan(_ =>
             {
-                //_.TheCallingAssembly(); // No the implementation of the interface is not in this assembly
-                _.WithDefaultConventions(); // Default Convention is that the interface has the same name as the implementation except for the I prefix
-                _.AssembliesAndExecutablesFromApplicationBaseDirectory(); // The implementation of the interface is in another assembly in the base directory
-                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("PR.Persistence"));
+                _.WithDefaultConventions();
+                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("Craft.Logging"));
+                _.AssembliesFromApplicationBaseDirectory(d => d.FullName.StartsWith("PR.IO"));
+                _.Assembly(repositoryPluginAssembly);
+                _.LookForRegistries();
             });
         }
     }
