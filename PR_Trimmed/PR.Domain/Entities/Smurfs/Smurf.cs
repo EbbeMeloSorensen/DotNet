7db4cf82a892f72e8b74106ba3dfe3fa6f212c2f
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-01-13 21:13:39 +0100

    -

---
+++ PR_Trimmed/PR.Domain/Entities/Smurfs/Smurf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PR.Domain.Entities.Smurfs
+{
+    public class Smurf
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
