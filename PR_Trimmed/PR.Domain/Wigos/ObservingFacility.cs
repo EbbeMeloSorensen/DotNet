96576ca4bd4fe2d18fb57f64bb1556fe4821d271
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-01-13 17:58:00 +0100

    -

--- PR_Trimmed/PR.Domain/Wigos/ObservingFacility.cs
+++ PR_Trimmed/PR.Domain/Wigos/ObservingFacility.cs
@@ -2,33 +2,6 @@
 
 namespace PR.Domain.Wigos
 {
-    public abstract class VersionedObject
-    {
-        public Guid ObjectId { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime Superseded { get; set; }
-
-        public VersionedObject(
-            Guid objectId,
-            DateTime created)
-        {
-            ObjectId = objectId;
-            Created = created;
-            Superseded = DateTime.MaxValue;
-        }
-    }
-
-    public abstract class AbstractEnvironmentalMonitoringFacility : VersionedObject
-    {
-        public Guid Id { get; set; }
-
-        protected AbstractEnvironmentalMonitoringFacility(
-            Guid objectId,
-            DateTime created) : base(objectId, created)
-        {
-        }
-    }
-
     public class ObservingFacility : AbstractEnvironmentalMonitoringFacility
     {
         public string? Name { get; set; }
@@ -44,49 +17,6 @@ namespace PR.Domain.Wigos
         public override string ToString()
         {
             return $"Observing Facility: {Name}";
-        }
-    }
-
-    public abstract class GeospatialLocation : VersionedObject
-    {
-        public Guid Id { get; set; }
-
-        public DateTime From { get; set; }
-        public DateTime To { get; set; }
-
-        public Guid AbstractEnvironmentalMonitoringFacilityId { get; set; }
-        public Guid AbstractEnvironmentalMonitoringFacilityObjectId { get; set; }
-        public virtual AbstractEnvironmentalMonitoringFacility AbstractEnvironmentalMonitoringFacility { get; set; }
-
-        public GeospatialLocation(
-            Guid objectId,
-            DateTime created) : base(objectId, created)
-        {
-        }
-    }
-
-    public class Point : GeospatialLocation
-    {
-        public string CoordinateSystem { get; set; }
-        public double Coordinate1 { get; set; }
-        public double Coordinate2 { get; set; }
-
-        public Point(
-            Guid objectId,
-            DateTime created) : base(objectId, created)
-        {
-        }
-
-        public override string ToString()
-        {
-            var result = $"({Coordinate1}, {Coordinate2}), From: {From.ToShortDateString()}";
-
-            if (To < DateTime.MaxValue)
-            {
-                result += $" To: {To.ToShortDateString()}";
-            }
-
-            return result;
         }
     }
 }
