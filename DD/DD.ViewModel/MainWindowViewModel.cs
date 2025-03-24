21263dd08a426ed9a2e0899c2006b233bc536bde
EbbeMeloSorensen <ebs@dmi.dk>
2025-03-24 08:35:19 +0100

    -

--- DD/DD.ViewModel/MainWindowViewModel.cs
+++ DD/DD.ViewModel/MainWindowViewModel.cs
@@ -57,11 +57,11 @@ namespace DD.ViewModel
             var creatureDiameter = 75;
             var projectileDiameter = 75;
 
-            var engine = new ComplexEngine(_application.Logger);
-            //var engine = new SimpleEngine(_application.Logger);
+            //var engine = new ComplexEngine(_application.Logger);
+            var engine = new SimpleEngine(_application.Logger);
 
-            //engine.BoardTileMode = BoardTileMode.Hexagonal;
-            engine.BoardTileMode = BoardTileMode.Square;
+            engine.BoardTileMode = BoardTileMode.Hexagonal;
+            //engine.BoardTileMode = BoardTileMode.Square;
 
             _application.Engine = engine;
 
